ae2fa17fe2012d303891b6ca12735c03ac719440
David Kiama <davidkiama98@gmail.com>
2020-10-29 18:53:38 +0300

    Initial barebone structure of Vendalls products

---
+++ app.js
@@ -0,0 +1,22 @@
+const products = document.querySelectorAll(".prod__img");
+const prodBg = document.querySelectorAll(".product__Bg");
+
+let x = window.matchMedia("(max-width:1400px)");
+
+function changeHeight() {
+  for (i of prodBg) {
+    for (h of products) {
+      if (x.matches) {
+        let prodHeight = h.offsetHeight;
+
+        i.style.height = `${prodHeight}px`;
+        console.log(i);
+      } else {
+        i.style.height = "232px";
+      }
+    }
+  }
+}
+
+changeHeight();
+window.addEventListener("resize", changeHeight);
